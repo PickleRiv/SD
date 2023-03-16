c393b3933fe0d04d64e872fe5ee93d248e56b5e8
PickleRiv <jprw360@gmail.com>
2023-03-16 19:50:14 +0000

    extra

--- Assignment_1/HeistToTheMuseum/sharedRegions/ConcentrationSite.java
+++ Assignment_1/HeistToTheMuseum/sharedRegions/ConcentrationSite.java
@@ -4,7 +4,6 @@ import entities.*;
 
 public class ConcentrationSite {
     
-<<<<<<< HEAD
     public synchronized boolean amINeeded(){
         ((OrdinaryThief) Thread.currentThread()).setThiefState(OrdinaryThiefStates.CONCENTRATION_SITE);
         return true;
@@ -15,22 +14,6 @@ public class ConcentrationSite {
 
     public synchronized void prepareExcursion(){
         ((OrdinaryThief) Thread.currentThread()).setThiefState(OrdinaryThiefStates.CRAWLING_INWARDS);
-=======
-    public synchronized int appraiseSit(){
-        return 0;
-    }
-
-    public synchronized boolean amINeeded(){
-        return true;
-    }
-
-    public synchronized int prepareAssaultParty(){
-        return 0;
-    }
-
-    public synchronized int prepareExcursion(){
-        return 0;
->>>>>>> ff1122621875153113e35aab030ec45b969110d3
     }
 
     public synchronized void sumUpResults(){
